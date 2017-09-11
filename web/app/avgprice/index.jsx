12ce8434fc09d85b4d9e29e511ed95f2b24b4cf7
wangwei703 <wangwei703@163.com>
2017-09-11 21:40:42 +0800

    添加日均统计的分布图

--- web/app/avgprice/index.jsx
+++ web/app/avgprice/index.jsx
@@ -4,33 +4,43 @@ import AvgPrice from "containers/charts/avgprice";
 import Community from "../cfg/comm.js";
 
 class componentName extends Component {
-     state = {
-        community: []
+    state = {
+        community: [],
+        list: []
     }
-    componentDidMount() {
+    loadAvgData() {
         var url = `./data/avg.json?_dc=${new Date().getTime()}`;
         fetch(url, {
             method: 'GET'
         }).then(response => {
             if (response.ok) {
                 response.json().then(json => {
-                    let comm = [];
-                    comm = Community.map(c => {
-                        let data = json.data.filter(h => h.c === c.key),
-                            date = json.date;
-                        date.sort((d1, d2) => d1 > d2 ? 1 : (d1 < d2 ? -1 : 0));
-                        return {
-                            key: c.key,
-                            name: c.name,
-                            data: {
-                                data,
-                                date,
-                                source: json.source
+                    this.loadListData().then(listJson => {
+                        let list = listJson.data || [];
+                        let comm = [];
+                        comm = Community.map(c => {
+                            let data = json.data.filter(h => h.c === c.key),
+                                date = json.date,
+                                houseData=list.find(i=>i.key===c.key),
+                                listData=[];
+                            if(houseData){
+                                listData=houseData.source||[];
                             }
-                        }
-                    })
-                    this.setState({
-                        community: comm
+                            date.sort((d1, d2) => d1 > d2 ? 1 : (d1 < d2 ? -1 : 0));
+                            return {
+                                key: c.key,
+                                name: c.name,
+                                data: {
+                                    data,
+                                    date,
+                                    source: json.source,
+                                    list:listData
+                                }
+                            }
+                        })
+                        this.setState({
+                            community: comm
+                        });
                     });
                 });
             } else {
@@ -40,12 +50,27 @@ class componentName extends Component {
             message.error(typeof e === "string" ? e : '加载数据文件失败');
         });
     }
+    loadListData() {
+        var url = `./data/list.json?_dc=${new Date().getTime()}`;
+        return fetch(url, {
+            method: 'GET'
+        }).then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            return {};
+        });
+    }
+    componentDidMount() {
+        this.loadAvgData();
+        this.loadListData();
+    }
     render() {
         return (
             <div className="report-main">
                 {
                     this.state.community.map(c => {
-                        return <AvgPrice key={c.key} name={c.name} rptdata={c.data} style={{minHeight:"30rem"}}/>
+                        return <AvgPrice key={c.key} name={c.name} rptdata={c.data} style={{ minHeight: "30rem" }} />
                     })
                 }
             </div>
--- web/containers/charts/avgprice.jsx
+++ web/containers/charts/avgprice.jsx
@@ -5,9 +5,11 @@ import ChartBase from "./chartbase";
 
 class AvgPrice extends ChartBase {
     format(rptData) {
+        console.log(rptData);
         let source = Object.entries(rptData.source);
-        let l = [], x = [], s = [];
+        let l = [], list = [], x = [], s = [], scatter = [];
         x = rptData.date;
+        list = rptData.list || [];
         l = source.map(s => s[1]);
         source.forEach(_s => {
             let _d = [];
@@ -18,31 +20,29 @@ class AvgPrice extends ChartBase {
                 } else {
                     _d.push(null);
                 }
-            })
+            });
+            let nlist = list.find(_h => _h.from === _s[0]);
+            if (nlist) {
+                let scatterData = nlist.house.map(_h => [_h[3], _h[2]]);
+                scatter.push({
+                    name: _s[1],
+                    data: scatterData
+                });
+            }
             s.push({
                 name: _s[1],
                 data: _d
             });
         });
-        return { l, x, s };
+        return { l, x, s, scatter };
     }
     renderChart() {
-        let { name, rptdata } = this.props;
+        let { name, rptdata, scatter } = this.props;
         let options = this.buildChartOptions(name, rptdata);
         this.myChart && this.myChart.setOption(options);
     }
     buildChartOptions(title, data) {
-        let { x, l, s } = this.format(data);
-        let scatter =[];
-         x.forEach(_x => {
-            let rnd = Math.round(Math.random() * 100);
-            for (let i = 0; i < rnd; i++) {
-                scatter.push({
-                    value:[_x, 10000 + Math.round(Math.random() * 10000)],
-                    symbolOffset:[2*(3-i%5),0]
-                });
-            }
-        });
+        let { x, l, s, scatter } = this.format(data);
         let series = s.map(getLineSeries),
             xAxis = getX({
                 data: x
@@ -54,27 +54,52 @@ class AvgPrice extends ChartBase {
                     formatter: '{value} ￥'
                 }
             })];
-        series.push({
-            name:"安居客",
-            type: 'scatter',
-            data: scatter,
-            symbolSize: 2,
-            hoverAnimation:false,
-            legendHoverLink:false,
-            silent:true,
-            itemStyle: {
-                normal: {
-                    opacity:.8
+        scatter.forEach(item => {
+            let data = item.data.map((d, i) => ({
+                value: d,
+                symbolOffset: [3 * (3 - i % 5), 0]
+            }));
+            series.push({
+                name: item.name,
+                type: 'scatter',
+                data,
+                symbolSize: 3,
+                hoverAnimation: false,
+                legendHoverLink: false,
+                silent: true,
+                itemStyle: {
+                    normal: {
+                        opacity: .8
+                    }
                 }
-            }
+            });
         });
-        console.log(series);
+
         let opts = getOptions({
             title: getTitle(title),
             toolbox: getToolbox(),
             legend: getLegend({
                 data: l
             }),
+            tooltip: {
+                axisPointer: {
+                    type: 'shadow'
+                },
+                formatter(a, b, c) {
+                    let title = a[0].axisValue,
+                        line = a.filter(_a => _a.seriesType === "line"),
+                        scatter = a.filter(_a => _a.seriesType === "scatter");
+                    return `<b style="padding:8px 0">${title}</b><br />
+                            ${line.map(l => {
+                            let val = typeof l.value === "number" ? l.value : "--";
+                            return "<div style='padding:2px 0'>" + l.marker + l.seriesName + "：" + val + "</div>"
+                        }).join("")}
+                            <div style='padding:2px 0'>
+                                <span style="display:inline-block;margin-right:5px;border-radius:2px;width:9px;height:9px;background-color:#666;"></span>
+                                房源数：${scatter.length}
+                            </div>`
+                }
+            },
             xAxis,
             yAxis,
             series
